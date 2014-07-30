a8819b9f93b16c3969e12a1953131c5c4e249ac3
lstoars <lstoars@gmail.com>
2014-07-30 15:24:35 +0800

    修改注释

--- coo.core/src/coo/core/jackson/JsonIEnumSerializer.java
+++ coo.core/src/coo/core/jackson/JsonIEnumSerializer.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import coo.core.model.IEnum;
 
 /**
- * Created by hechengfei on 2014/7/30.
+ * json 格式下的 IEnum 类型的序列化类
  */
 public class JsonIEnumSerializer extends JsonSerializer<IEnum> {
 
