6542157ebc684a8797576f22af279abb9b6ff576
Jnoee <jnoee@qq.com>
2014-09-04 20:02:25 +0800

    增加黑色。

--- coo.mvc/src/coo/mvc/constants/StatusColor.java
+++ coo.mvc/src/coo/mvc/constants/StatusColor.java
@@ -4,12 +4,14 @@ package coo.mvc.constants;
  * 状态颜色。
  */
 public abstract class StatusColor {
+	/** 黑色（通常用来标识原始状态） */
+	public static final String BLACK = "black";
 	/** 绿色（通常用来标识正常状态） */
 	public static final String GREEN = "green";
-	/** 绿色（通常用来标识提醒和警告状态） */
+	/** 橙色（通常用来标识提醒和警告状态） */
 	public static final String ORANGE = "orange";
-	/** 绿色（通常用来标识失效状态） */
+	/** 灰色（通常用来标识失效状态） */
 	public static final String GRAY = "gray";
-	/** 绿色（通常用来标识异常状态） */
+	/** 红色（通常用来标识异常状态） */
 	public static final String RED = "red";
 }
