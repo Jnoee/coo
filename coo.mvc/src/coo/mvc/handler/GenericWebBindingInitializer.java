2f814ac1f0f464e65b8e5acf06d76b6ebc6806a2
Jnoee <jnoee.he@gmail.com>
2015-03-04 15:10:58 +0800

    修改配置问题。

--- coo.mvc/src/coo/mvc/handler/GenericWebBindingInitializer.java
+++ coo.mvc/src/coo/mvc/handler/GenericWebBindingInitializer.java
@@ -1,15 +1,17 @@
 package coo.mvc.handler;
 
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.support.WebBindingInitializer;
+import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 import org.springframework.web.context.request.WebRequest;
 
 /**
  * 常规数据绑定初始化器。
  */
-public class GenericWebBindingInitializer implements WebBindingInitializer {
+public class GenericWebBindingInitializer extends
+		ConfigurableWebBindingInitializer {
 	@Override
 	public void initBinder(WebDataBinder binder, WebRequest request) {
+		super.initBinder(binder, request);
 		binder.setAutoGrowCollectionLimit(Integer.MAX_VALUE);
 	}
 }
