9ccaf3f5f1240731551cdddfc04c5fb4a781fb57
Jnoee <jnoee.he@gmail.com>
2015-03-14 01:39:14 +0800

    增加方法。

--- coo.core/src/coo/core/hibernate/cache/EntityCacheManager.java
+++ coo.core/src/coo/core/hibernate/cache/EntityCacheManager.java
@@ -28,7 +28,7 @@ public class EntityCacheManager extends EntityClassBeanFactoryPostProcessor {
 	}
 
 	/**
-	 * 清空指定实体类的缓存。
+	 * 清空指定实体缓存。
 	 * 
 	 * @param entityClasses
 	 *            实体类列表
@@ -37,6 +37,34 @@ public class EntityCacheManager extends EntityClassBeanFactoryPostProcessor {
 		for (Class<?> entityClass : entityClasses) {
 			sessionFactory.getCache().evictEntityRegion(entityClass);
 		}
+	}
+
+	/**
+	 * 清空所有实体缓存。
+	 */
+	public void evictEntityRegions() {
+		sessionFactory.getCache().evictEntityRegions();
+	}
+
+	/**
+	 * 清空所有集合缓存。
+	 */
+	public void evictCollectionRegions() {
+		sessionFactory.getCache().evictCollectionRegions();
+	}
+
+	/**
+	 * 清空所有查询缓存。
+	 */
+	public void evictQueryRegions() {
+		sessionFactory.getCache().evictQueryRegions();
+	}
+
+	/**
+	 * 清空所有缓存。
+	 */
+	public void evictAllRegions() {
+		sessionFactory.getCache().evictAllRegions();
 	}
 
 	public List<Class<?>> getCachedEntityClasses() {
