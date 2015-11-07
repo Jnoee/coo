a5b01863040a428c8be825f82b84df8cd946374d
hesong <jnoee@qq.com>
2015-11-07 17:50:18 +0800

    修改样式。

--- coo.mvc/src/coo/mvc/static/dwz/dwz.js
+++ coo.mvc/src/coo/mvc/static/dwz/dwz.js
@@ -4201,14 +4201,17 @@ var navTab = {
 				var $grid = $this.parent(); // table
 				var nowrap = $this.hasClass("nowrap");
 
-				$trs.hoverClass("hover").each(function(index) {
+				if(!nowrap) $trs.hoverClass("hover");
+				$trs.each(function(index) {
 					var $tr = $(this);
 					if(!nowrap && index % 2 == 1)
 						$tr.addClass("trbg");
 
 					$tr.click(function() {
-						$trs.filter(".selected").removeClass("selected");
-						$tr.addClass("selected");
+						if(!nowrap) {
+							$trs.filter(".selected").removeClass("selected");
+							$tr.addClass("selected");
+						}
 						var sTarget = $tr.attr("target");
 						if(sTarget) {
 							if($("#" + sTarget, $grid).size() == 0) {
