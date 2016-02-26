97812a65b1c8597a1f579adcbfa42bce897108e9
jnoee <jnoee.he@gmail.com>
2016-02-26 18:18:01 +0800

    增加LocalTime用户自定义类型。

--- coo.core/src/coo/core/hibernate/usertype/package-info.java
+++ coo.core/src/coo/core/hibernate/usertype/package-info.java
@@ -8,7 +8,8 @@
 		@TypeDef(name = "Array", typeClass = ArrayUserType.class),
 		@TypeDef(name = "ArrayList", typeClass = ArrayListUserType.class),
 		@TypeDef(name = "UuidEntity", typeClass = UuidEntityUserType.class),
-		@TypeDef(name = "UuidEntityList", typeClass = UuidEntityListUserType.class) })
+		@TypeDef(name = "UuidEntityList", typeClass = UuidEntityListUserType.class),
+		@TypeDef(name = "LocalTime", typeClass = LocalTimeUserType.class) })
 package coo.core.hibernate.usertype;
 
 import org.hibernate.annotations.TypeDef;
