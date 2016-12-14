9ac3ac72c2051c5b77ce9da014392850a4192471
jnoee <jnoee.he@gmail.com>
2016-12-14 14:51:02 +0800

    增加短信组件支持。

---
+++ coo.core.support/src/coo/core/sms/esms/EsmsConfig.java
@@ -0,0 +1,34 @@
+package coo.core.sms.esms;
+
+/**
+ * Esms短信组件配置。
+ */
+public class EsmsConfig {
+  private String url;
+  private String username;
+  private String password;
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
